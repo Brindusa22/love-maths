663d7442190f32ed46b96c7f9da1ebd653a1d386
Brindusa22 <>
2023-09-21 13:07:13 +0000

    Correct typo

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,13 +32,10 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
-
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
-
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`
@@ -83,7 +80,7 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
 
     } else if (operator === "-") {
-        return [operand1 - operand2, "subrtact"];
+        return [operand1 - operand2, "subtract"];
 
     } else {
         alert(`Unimplemented operator ${operator}`);
